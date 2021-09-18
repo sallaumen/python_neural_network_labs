d5d23420e66fa7e27d893d296c50dafbd4b3970e
Lucas Tavano <lc_tavano@hotmail.com>
2021-09-18 17:31:49 -0300

    CIFAR10 implementation improvements

--- lib/implementations/CIFAR10/cifar_10.py
+++ lib/implementations/CIFAR10/cifar_10.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-# %matplotlib inline
-from tensorflow.keras import datasets, layers, models
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-from keras.datasets import cifar10
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import cifar10
+import time
 
 np.random.seed(10)
+tf.compat.v1.disable_eager_execution()
+
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
+np.random.seed(10)
+
 
 def plot_sample(x, y, index):
     plt.figure(figsize=(15, 2))
     plt.imshow(x[index])
     plt.xlabel(classes[y[index]])
+
 
 def plot_range(x_train, y_train, classes):
     num_classes = 10
@@ -23,7 +30,8 @@ def plot_range(x_train, y_train, classes):
         ax[i].imshow(sample)
         ax[i].set_title("Label: {}".format(classes[y_train[i]]), fontsize=16)
 
-"""#DATA"""
+
+"""# DATA """
 print("\n\nGetting data")
 # Classes:
 # airplane, automobile, bird, cat, deer, dog, frog, horse, ship, and truck
@@ -53,8 +61,7 @@ x_test = x_test / 255.0
 
 print("\n\nModel - FCNN ")
 # Modelo BOM CNN
-model = models.Sequential()
-cnn = models.Sequential([
+model = models.Sequential([
     layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
 
@@ -66,12 +73,17 @@ cnn = models.Sequential([
     layers.Dense(10, activation='softmax')
 ])
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer='SGD', metrics='accuracy')
+model.compile(loss='sparse_categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+# model.build(input_shape)
 model.summary()
 
 """# Train"""
 print("\n\nTrain")
-model.fit(x=x_train, y=y_train, epochs=10)
+start_training_time = time.time()
+epochs = 1
+model.fit(x=x_train, y=y_train, epochs=epochs)
+end_training_time = time.time()
+print("Total training time using {0} epochs: {1} seconds".format(epochs, end_training_time - start_training_time))
 
 """# Evaluate"""
 print("\n\nEvaluate")
@@ -95,32 +107,3 @@ y_sample_pred_class = y_pred_classes_str[random_idx]
 
 plt.title("Predicated: {}, True: {}".format(y_sample_pred_class, y_sample_true, frozenset=16))
 plt.imshow(x_sample.reshape(32, 32, 3))
-
-"""# Investigate Some Errors"""
-
-def investigate_errors():
-    global i
-    print(y_pred_classes, y_true)
-    errors = (y_pred_classes != y_true)
-    y_pred_classes_errors = y_pred_classes[errors]
-    y_pred_errors = y_pred[errors]
-    y_true_errors = y_true[errors]
-    x_test_errors = x_test[errors]
-    y_pred_errors_probability = np.max(y_pred_errors, axis=1)
-    true_probability_errors = np.diagonal(np.take(y_pred_errors, y_true_errors, axis=1))
-    diff_errors_pred_true = y_pred_errors_probability - true_probability_errors
-    # Get list of indices of sorted differences
-    sorted_idx_diff_errors = np.argsort(diff_errors_pred_true)
-    top_idx_diff_errors = sorted_idx_diff_errors[-5:]  # 5 last ones
-    # Show Top Errors
-    num = len(top_idx_diff_errors)
-    f, ax = plt.subplots(1, num, figsize=(30, 30))
-    for i in range(0, num):
-        idx = top_idx_diff_errors[i]
-        sample = x_test_errors[idx].reshape(28, 28)
-        y_t = y_true_errors[idx]
-        y_p = y_pred_classes_errors[idx]
-        ax[i].imshow(sample, cmap='gray')
-        ax[i].set_title("Predicted label :{}\nTrue label: {}".format(y_p, y_t), fontsize=22)
-
-# investigate_errors()
